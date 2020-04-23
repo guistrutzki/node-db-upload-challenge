4d8fadc0e7fb6629d98dc108d65bd50fb9e5d91e
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2020-04-23 01:29:12 -0300

    :sparkles: Add list and create transaction

--- src/database/migrations/1587524904586-AddCategoryIdToTransactions.ts
+++ src/database/migrations/1587524904586-AddCategoryIdToTransactions.ts
@@ -9,7 +9,7 @@ export default class AddCategoryIdToTransactions1587524904586
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.addColumn(
-      'transaction',
+      'transactions',
       new TableColumn({
         name: 'category_id',
         type: 'uuid',
